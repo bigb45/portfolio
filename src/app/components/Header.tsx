aa018d5fddb75b4d18c3f79081549b9f4ce75007
Mohammed <bgd4500@gmail.com>
2025-01-03 15:10:01 +0200

    Feature: add squiggly line under header navigation button

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,12 +5,14 @@ function Header() {
     <div className="py-2 flex justify-between items-center min-w-full h-20 backdrop-blur-md fixed px-4 lg:px-80 z-10 w-full">
       <div className="text-3xl flex-grow text-center lg:text-left">PORTOFILO</div>
       <div className="gap-4 flex flex-row hidden sm:hidden md:hidden lg:flex">
-        <Link href="/" className="hover:underline decoration-wavy">
+        <Link href="/" 
+        className="squiggle"
+        >
           Home
         </Link>
-        <Link href="#about">About</Link>
-        <Link href="/">Work</Link>
-        <Link href="/">Experience</Link>
+        <Link href="#about" className="squiggle">About</Link>
+        <Link href="/" className="squiggle">Work</Link>
+        <Link href="/" className="squiggle">Experience</Link>
       </div>
     </div>
   );
