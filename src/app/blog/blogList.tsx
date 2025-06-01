596694d127461340c771c7cf1d0670b9d61a42c8
natour <bgd4500@gmail.com>
2025-06-01 22:47:57 +0300

    Fix: Convert blogList to client component to fetch blogs on each load

--- src/app/blog/blogList.tsx
+++ src/app/blog/blogList.tsx
@@ -1,26 +1,30 @@
+'use client';
 import React from "react";
 import BlogListItem, { BlogListItemProps } from "../components/BlogItem";
 import { prisma } from "@/lib/prisma";
 
-export default async function BlogList() {
-    const blogList = await getBlogs();
+
+import { useEffect, useState } from 'react';
+import { BlogObject } from "./[blogPostId]/page";
+
+export default function BlogList() {
+    const [blogs, setBlogs] = useState<BlogListItemProps[]>([]);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            const res = await fetch('/api/blog'); // calls your API route
+            const data = await res.json();
+            setBlogs(data);
+        };
+
+        fetchBlogs();
+    }, []);
+
     return (
         <div>
-            {blogList.map((blogItem) => {
-                return <BlogListItem key={blogItem.id} {...blogItem} />;
-            })}
+            {blogs.map((blogItem) => (
+                <BlogListItem key={blogItem.id} {...blogItem} />
+            ))}
         </div>
     );
-}
-
-async function getBlogs(): Promise<BlogListItemProps[]> {
-
-    const blog = await prisma.blog.findMany({
-        orderBy: [
-            { isPinned: 'desc' },
-            { publishDate: 'desc' }
-        ]
-    })
-
-    return blog
 }
