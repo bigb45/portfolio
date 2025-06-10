e1b2a2da4a74241a853b7f64067dc518713d37c6
natour <bgd4500@gmail.com>
2025-06-10 12:39:39 +0300

    Fix: remove NEXT_PUBLIC from env var name used in server component

--- src/app/blog/blogList.tsx
+++ src/app/blog/blogList.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import BlogListItem, { BlogListItemProps } from "../components/BlogItem";
 import BlogSkeleton from "./blogSkeleton";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function BlogList() {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+    const baseUrl = process.env.SITE_URL || "http://localhost:3000";
 
     const res = await fetch(`${baseUrl}/api/blog`);
     const blogs = await res.json();
 
     return (
         <div>
-            {blogs ?
+            {blogs ? (
                 blogs.map((blogItem: BlogListItemProps) => (
                     <BlogListItem key={blogItem.id} {...blogItem} />
-                )) : <BlogSkeleton />
-
-            }
+                ))
+            ) : (
+                <BlogSkeleton />
+            )}
         </div>
     );
 }
