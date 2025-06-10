eb4633b4a717389678a9429e8059a613802f31fc
natour <bgd4500@gmail.com>
2025-06-10 09:48:37 +0300

    Refactor: change font from space grotesk to Familjen grotesk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,42 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./nodemodules/ @yourstorybooklib/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-        w95fa: ["w95fa", "sans-serif"],
-        grotesk: ['"Space Grotesk"', "Inter"],
-      },
-      colors: {
-        background: "#b8b8b8",
-        foreground: "#a0beb4",
-        accent: "#000072",
-        "accent-light": "#AECAE2",
-        "on-accent": "#ffffff",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./nodemodules/ @yourstorybooklib/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                "2xl": "1536px",
+            },
+
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+                w95fa: ["w95fa", "sans-serif"],
+                grotesk: ['"Familjen Grotesk"', "Inter"],
+            },
+            colors: {
+                background: "#b8b8b8",
+                foreground: "#a0beb4",
+                accent: "#000072",
+                "accent-light": "#AECAE2",
+                "on-accent": "#ffffff",
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
