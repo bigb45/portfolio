23baf6cf0e0e60bf22c4967e6d0038784c1f4d6d
natour <bgd4500@gmail.com>
2025-07-04 11:32:35 +0300

    Feature: add utility to convert hex to rgb

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,36 +1,41 @@
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 export function formatDate(date: Date): string {
-  const formatter = Intl.DateTimeFormat("en-US", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric"
-  ,
-
-  })
+    const formatter = Intl.DateTimeFormat("en-US", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
     // const day = date.getDate().toString().padStart(2, '0')
     // const month = date.toLocaleString('en-US', { month: 'short' })
     // const year = date.getFullYear().toString().slice(-2)
 
     // const formattedDate = `${day}, ${month} '${year}`
-    return formatter.format(new Date(date))
+    return formatter.format(new Date(date));
 }
 
-
-// method and interface to convert BigInt to String 
+// method and interface to convert BigInt to String
 // fixes "TypeError: Do not know how to serialize a BigInt" error
 declare global {
     interface BigInt {
-      toJSON(): string;
+        toJSON(): string;
     }
-  }
-  
-  BigInt.prototype.toJSON = function () {
-    return this.toString();
-  };
+}
 
+BigInt.prototype.toJSON = function () {
+    return this.toString();
+};
 
 export function generateRandomId(): string {
-    return uuidv4()
-  }
+    return uuidv4();
+}
+
+export function hexToRgb(hex: string) {
+    const cleanHex = hex.replace("#", "").slice(0, 6);
+    const bigint = parseInt(cleanHex, 16);
+    return {
+        r: (bigint >> 16) & 255,
+        g: (bigint >> 8) & 255,
+        b: bigint & 255,
+    };
+}
