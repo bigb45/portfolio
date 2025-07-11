ca95946889f8a065d0a4fb24758741ac70d74bbf
natour <bgd4500@gmail.com>
2025-07-11 20:49:57 +0300

    Feature: add loading animations

---
+++ src/components/ui/loadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LoadingSpinner() {
+    return (
+        <div className="loader-grid">
+            <div className="square s0"></div>
+            <div className="square s1"></div>
+            <div className="square s3"></div>
+            <div className="square s2"></div>
+        </div>
+    );
+}
