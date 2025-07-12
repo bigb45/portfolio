b063dd8e0fe8f589eb8143086c43d830a60f189a
natour <bgd4500@gmail.com>
2025-07-12 03:13:02 +0300

    Feature: add navigation controls to small and medium screens

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,11 +7,27 @@ function Header() {
             <div className="z-10 mx-auto flex h-20 w-full max-w-2xl items-center justify-between lg:max-w-4xl">
                 <Link
                     href="/"
-                    className="font-space flex-grow text-center text-[48px] lg:text-left"
+                    className="flex-grow text-center font-space text-[48px] lg:text-left"
                 >
                     Natour
                 </Link>
                 <div className="hidden flex-row gap-4 lg:flex">
+                    <Link href="/blog" className="squiggle text-[18px]">
+                        Blog
+                    </Link>
+                    <Link href="/case-studies" className="squiggle text-[18px]">
+                        Case studies
+                    </Link>
+                    <Link href="/projects" className="squiggle text-[18px]">
+                        Projects
+                    </Link>
+                    <Link href="/" className="squiggle text-[18px]">
+                        Secret
+                    </Link>
+                </div>
+            </div>
+            <div className="flex w-full justify-around py-3 lg:hidden">
+                <div className="flex gap-3 text-[16px] font-medium text-gray-700">
                     <Link href="/blog" className="squiggle">
                         Blog
                     </Link>
