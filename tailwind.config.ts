05930fd57374df7daabcae3322db590b980bde72
natour <bgd4500@gmail.com>
2025-08-22 21:33:05 +0300

    Fix: fix css style issue caused by mismatch between postcss and tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/**/*.{js,jsx,ts,tsx}",
-        "./nodemodules/ @yourstorybooklib/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {
